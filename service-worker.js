9bc894da75bc5635f671f2d56f6baf7d338a3dbd
Caesar <Caesar@kekxv.com>
2019-04-30 14:07:46 +0800

    fix bug

--- service-worker.js
+++ service-worker.js
@@ -47,7 +47,7 @@ let filesToCache = [
 self.addEventListener('install', function (e) {
     console.log('[ServiceWorker] Install');
     e.waitUntil(
-        caches.open(cacheName).then(function (cache) {
+        caches.open(CACHE_NAME).then(function (cache) {
             console.log('[ServiceWorker] Caching app shell');
             return cache.addAll(filesToCache);
         }));
